3d98ce0fc6823fab29f7484bc83c04d0636e4088
Cristian Díaz <crisdi384@hotmail.com>
2021-08-01 11:38:24 -0500

    Final layout design of blog

---
+++ src/js/utils/blogContentAnimation.js
@@ -0,0 +1,39 @@
+const NAV_BUTTON = document.querySelector(".navbar__button");
+const CONTENT_MENU = document.querySelector(".blog-navbar");
+const CONTENT_LINKS = document.querySelectorAll(".blog-navbar li");
+const CONTENT_MENU_SHADOW = document.querySelector(".blog-navbar__shadow");
+const HTML = document.getElementsByTagName("html");
+
+const addActiveClass = function () {
+  NAV_BUTTON.classList.toggle("active");
+  CONTENT_MENU.classList.toggle("active");
+};
+
+const switchOverflowHTML = function () {
+  if (NAV_BUTTON.classList.contains("active")) {
+    HTML[0].style.overflow = "hidden";
+  } else {
+    HTML[0].style.overflow = "visible";
+  }
+};
+
+const addClassTrigger = function () {
+  NAV_BUTTON.addEventListener("click", function () {
+    addActiveClass();
+    switchOverflowHTML();
+  });
+
+  CONTENT_MENU_SHADOW.addEventListener("click", function () {
+    addActiveClass();
+    switchOverflowHTML();
+  });
+
+  CONTENT_LINKS.forEach((element) => {
+    element.addEventListener("click", function () {
+      addActiveClass();
+      HTML[0].style.overflow = "visible";
+    });
+  });
+};
+
+addClassTrigger();
